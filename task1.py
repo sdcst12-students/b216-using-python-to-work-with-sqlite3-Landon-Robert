3a5ebb3de9c52f87de2e3a906fb2667aa142f9cf
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2025-02-11 11:34:34 -0800

    worked on task 1

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 #!python
+import sqlite3
 
 """
 Create a query to create a table to store customers information into a database for a veterinarian
@@ -19,3 +20,29 @@ Fred Jones      fred@city.com   6045553434      102     75 57 Street            
 Leroy Jenkins   leroy@wow.ca    2342222323      103     65 Blizzard Ave         100     
 Jen Mezei       jen@shaw.ca     6042231134      104     891 Cullen Cresc        0
 """
+
+file = 'dbase.db'
+connection = sqlite3.connect(file)
+print(connection)
+
+cursor = connection.cursor()
+query = """
+create table if not exists customers (
+    name tinytext,
+    email tinytext,
+    phoneNumber int,
+    ID tinyint,
+    address tinytext,
+    balance tinytext);
+"""
+cursor.execute(query)
+data = [
+    ["Joe Smith", "joe@gmail.com", 7783341111, 101, "1234 Sesame Street", 0],
+    ["Fred Jones", "fred@city.com", 6045553434, 102, "75 57 Street", 0],
+    ["Leroy Jenkins", "leroy@wow.ca", 2342222323, 103, "65 Blizzard Ave", 100],
+    ["Jen Mezei", "jen@shaw.ca", 6042231134, 104, "891 Cullen Cresc", 0]
+]
+for i in data:
+    query = f"insert into customers (name,email,phone number, ID, address, balance) values ('{i[0]}','{i[1]}',{i[2]},{i[3]},'{i[4]}',{i[5]});"
+    print(query)
+    cursor.execute(query)
